6445fc288aae7076bb5781777c27d209658f48bb
Rui Hu <rayhusme@gmail.com>
2018-03-17 12:14:37 -0600

    Added code for s3 triggle lambda

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  console.log("ss")
+  // console.log(JSON.stringify(context))
+  // console.log(JSON.stringify(event))
+  console.log(event.Records[0].eventName)
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
